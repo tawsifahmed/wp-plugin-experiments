102930df23bf5a1c7c076d26e86d9b38c2422516
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2024-11-21 03:08:12 +0600

    maps view in page update

--- geo-store-locator/inc/shortcodes.php
+++ geo-store-locator/inc/shortcodes.php
@@ -1,22 +1,32 @@
 <?php
 
-function geo_store_locator_map_shortcode () {
+function geo_store_locator_map_shortcode ($atts) {
     $html = '
+    <h4>Map ID = '.$atts['id'].'</h4>
     <div id="map" style="width: 900px; height: 400px;"></div>
 
     <script>
-    
-   
     var map = L.map("map").setView([51.505, -0.09], 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; <a href=\'https://www.openstreetmap.org/copyright\'>OpenStreetMap</a> contributors"
-    }).addTo(map);
+    }).addTo(map);';
 
-    L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-    .openPopup();
+    $maps = get_post_meta($atts['id'], 'maps', true);
+
+    if(is_array($maps) && !empty($maps)){
+        foreach($maps as $map){
+
+            $html .='L.marker(['. $map['latitude'] .', '.$map['longitude'].']).addTo(map)
+            .bindPopup("'.$map['$title'].'");';
+        }
+    }
+
+   
+    $html .='
     </script>
     ';
+
+    // return var_dump($map);
     return $html;
 }
 
