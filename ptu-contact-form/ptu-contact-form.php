84ef85c120e30be4f97e2faf13532c083499ecf4
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2024-11-23 23:38:08 +0600

    styles added to form > ptu-contact-form

--- ptu-contact-form/ptu-contact-form.php
+++ ptu-contact-form/ptu-contact-form.php
@@ -25,24 +25,34 @@ add_shortcode('ptu_contact_form', 'ptu_contact_form_shortcode');
 function ptu_contact_form_shortcode(){
     ob_start();
     ?>
-    <div class="ptu-contact-form">
-    <form method="post">
+    <div class="ptu-contact-form" style="width: 90%; margin: 0 auto;">
+    <form method="post" style="display: flex; flex-direction: column;">
         <label for="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name" required>
+        <input style="padding: 0 0 20px 0;" type="text" id="first_name" name="first_name" required>
 
         <label for="last_name">Last Name</label>
-        <input type="text" id="last_name" name="last_name" required>
+        <input style="padding: 0 0 20px 0;" type="text" id="last_name" name="last_name" required>
 
         <label for="subject">Subject</label>
-        <input type="text" id="subject" name="subject" required>
+        <input style="padding: 0 0 20px 0;" type="text" id="subject" name="subject" required>
 
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" required>
+        <input style="padding: 0 0 20px 0;" type="email" id="email" name="email" required>
 
         <label for="message">Message</label>
-        <textarea id="message" name="message" required></textarea>
+        <textarea style="padding: 0 0 30px 0;" id="message" name="message" required></textarea>
 
-        <button type="submit" name="ptu_contact_form_submit">Submit</button>
+        <button type="submit" name="ptu_contact_form_submit" 
+        style="text-decoration: none;
+            background: crimson;
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+            color: white;
+            font-weight: 700;
+            font-size: medium;
+            margin-top: 17px;"
+        >Submit</button>
         <small>* All fields are required.</small>
     </form>
     </div>
