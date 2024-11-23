075685090ce55389de0fe872874bcc355ba68c6c
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2024-11-23 21:08:36 +0600

    empty index file added to prevent directory listing

---
+++ ptu-contact-form/index.php
@@ -0,0 +1,3 @@
+<?php
+// ptu means 'put to use'
+// this empty index is for to prevent directory listing so that if someone try to visit the directory of this plugin, they will see a blank page instead of the list of files in the directory
