a2a056fcb46ca8923693e3f82f36f0cfbee682f6
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2024-11-23 21:05:54 +0600

    practical demo contact form plugin initiated

---
+++ ptu-contact-form/ptu-contact-form.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+* Plugin Name:       PTU Contact Form
+* Description:       A simple contact form plugin for practice purpose.
+* Version:           1.0.0
+* Author:            Tawsif Ahmed Riyad
+* Plugin URI:        https://github.com/tawsifahmed//wp-plugin-experiments/tree/main/ptu-contact-form
+* Requires at least: 5.2
+* Requires PHP:      8.0
+* Text Domain:       ptu-contact-form
+*
+* @package           ptu-contact-form
+*/
+
+
+// here 'ABSPATH' is a constant that is defined in wp core directory (wp-config.php), so we can use it to prevent direct access to the file
+if (!defined('ABSPATH')) {
+    exit;
+}
